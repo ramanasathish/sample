8ae4d0851deac154667278637f05c6ea61de566e
ramanasathish <40193381+ramanasathish@users.noreply.github.com>
2023-09-21 14:48:56 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+const str = {
+  "connectionString": "mongodb+srv://interview:welcome2byteridge@byteridge-hdrl6.mongodb.net/interview",
+  "secret": "JBJYBJ23jkj232kALKMLAK"
+}
+
+module.exports = str;
