ce2d31c75a88dcb81d29eb0a3e9e0602f5f06be2
ramanasathish <40193381+ramanasathish@users.noreply.github.com>
2024-01-25 14:00:49 +0530

    Update server.js
    commit from base

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const jwt = require('_helpers/jwt');
 const errorHandler = require('_helpers/error-handler');
 
+//comment line
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
